5932d3c8220453bda25201b7cbdbc7278bd6efce
Muhammad Muzammil Ansari <121820382+Muzammilansari82@users.noreply.github.com>
2023-05-30 13:47:29 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,23 @@
+print("Mark Sheet:)")
+computertotalmarks = 100
+sciencetotalmarks = 100
+mathstotalmarks = 100
+englishtotalmarks = 100
+computerobtmarks = 99
+scienceobtmarks = 70
+mathsobtmarks = 70
+englishobtmarks = 80
+total = computertotalmarks + sciencetotalmarks + mathstotalmarks + englishtotalmarks
+obtain = computerobtmarks + scienceobtmarks + mathsobtmarks + englishobtmarks
+print("Computer Marks", computerobtmarks, "out of", computertotalmarks)
+print("Science Marks", scienceobtmarks, "out of", sciencetotalmarks)
+print("Maths Marks", mathsobtmarks, "out of", mathstotalmarks)
+print("Engish Marks", englishobtmarks, "out of", englishtotalmarks)
+print("Obtained Marks", obtain, " out of Total Marks", total)
+percentage = obtain / total * 100
+a = percentage
+print("%.2f" % percentage)
+if percentage >= 70:
+    print("passed")
+else:
+    print("failed")
